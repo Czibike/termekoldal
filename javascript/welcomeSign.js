836db75eaaeb7a51f2a5fa3506bfe72f3286e5c7
Czibike <czibike03@gmail.com>
2024-04-23 21:36:46 +0200

    first commitwith real data

---
+++ javascript/welcomeSign.js
@@ -0,0 +1,20 @@
+let btn = document.getElementsByClassName("btn");
+let obj = [
+  document.getElementById("firstText"),
+  document.getElementById("secondText"),
+];
+for (let j = 0; j < btn.length; j++) {
+  btn[j].addEventListener("click", () => {
+    for (let i = 0; i < obj.length; i++) {
+      obj[i].classList.toggle("active");
+    }
+  });
+}
+
+window.onload = () => {
+  setInterval(() => {
+    for (let i = 0; i < obj.length; i++) {
+      obj[i].classList.toggle("active");
+    }
+  }, 5000);
+};
